eb3b277f5cf95fa386c3df7d86f8037cd07c4c33
Bruno Malhano <tecnico.bruno.malhano@gmail.com>
2023-08-28 20:55:21 -0300

    Update providers

--- providers.tf
+++ providers.tf
@@ -16,8 +16,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "b37e9503-eeb6-45a5-a7f6-352ed7732f9f"
-  tenant_id         = "c86cc034-5597-4a5b-827a-4bc5c49bec5c"
-  client_id         = "d56be6a2-cc8e-4621-9664-b862b1a93362"
-  client_secret     = "S0e8Q~lC61uj1aKLEO0l_Fi.ymroBnzupHT0QdCk"
+  subscription_id   = "{subscription_id}"
+  tenant_id         = "{tenant_id}"
+  client_id         = "{client_id}"
+  client_secret     = "{client_secret}"
 }
